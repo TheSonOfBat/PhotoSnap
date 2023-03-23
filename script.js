3c522e6d00c5c7e3dbbb3cc2d6305dc533267e17
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-03-23 11:20:23 +1100

    Functional Mobile Nav

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 let hamburgerMenu = document.getElementById("hamburgerMenu");
 let span1 = document.getElementById("span1");
 let span2 = document.getElementById("span2");
+let mobileNav = document.getElementById("mobileNav");
 hamburgerMenu.addEventListener(("click"),()=>{
-    hamburgerMenu.classList.toggle("hamburgerMenuActive")
+    hamburgerMenu.classList.toggle("hamburgerMenuActive");
+    mobileNav.classList.toggle("mobileNavActive")
     span1.classList.toggle("span1Active");
     span2.classList.toggle("span2Active");
 });
