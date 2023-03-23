6d26fb91e15591efed8a8afb5cf39e0bcac3f44a
TheSonOfBat34 <TheSonOfBat@gmail.com>
2023-03-23 19:22:26 +1100

    Added New Section Styles

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let span1 = document.getElementById("span1");
 let span2 = document.getElementById("span2");
 let mobileNav = document.getElementById("mobileNav");
 hamburgerMenu.addEventListener(("click"),()=>{
+    mobileNav.classList.toggle("mobileNavActive");
     hamburgerMenu.classList.toggle("hamburgerMenuActive");
-    mobileNav.classList.toggle("mobileNavActive")
     span1.classList.toggle("span1Active");
     span2.classList.toggle("span2Active");
 });
